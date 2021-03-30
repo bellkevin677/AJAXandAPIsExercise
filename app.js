d73626c8febef1d431a4eb3bdb052d20939acd84
bellkevin677 <bellkevin677@gmail.com>
2021-03-30 09:57:42 -0500

    without bonus

--- app.js
+++ app.js
@@ -6,11 +6,10 @@ const p2 = document.createElement('p');
 first.append(p1);
 first.append(p2);
 
-function question1() {const jokeJS1 = `{"id":18,"type":"programming","setup":"Why did the programmer quit his job?","punchline":"Because he didn't get arrays."}`;
-const set = JSON.parse(jokeJS1)
-p1.innetText = set.setup;
-};
-question1();
+const jokeJS1 = JSON.parse(`{"id":18,"type":"programming","setup":"Why did the programmer quit his job?","punchline":"Because he didn't get arrays."}`);
+
+p1.innerText = jokeJS1.setup;
+p2.innerText = jokeJS1.punchline;
 
 // 2
 const second = document.querySelector('#second');
@@ -19,26 +18,51 @@ const p4 = document.createElement('p');
 second.append(p3);
 second.append(p4);
 
-async function displayRandomJoke1(){
-    try {
-        const joke = await axios.get(`https://official-joke-api.appspot.com/random_joke`);
-        console.log(`Axios Example 3 Was Successful`);
-        // Displays some data on the webpage
-        h1.innerText = joke.data.setup
-        h2.innerText = joke.data.punchline
-    } catch (err) {
-        console.log(`Axios Exampe 3 Was NOT Successful`);
+axios.get(`https://official-joke-api.appspot.com/jokes/random`)
+    .then(res => {
+        const jokeJS2 = res;
+        p3.innerText = jokeJS2.data.setup;
+        p4.innerText = jokeJS2.data.punchline;
+    })
+    .catch(err => {
         console.log(err);
-    }
-}
-displayRandomJoke1();
+    })
+
 // 3
 const third = document.querySelector('#third');
 const p5 = document.createElement('p');
 const p6 = document.createElement('p');
 third.append(p5);
 third.append(p6);
+
+async function jokeFunc(){
+    try {
+        const jokeJS3 = await axios.get(`https://official-joke-api.appspot.com/jokes/random`)
+        p5.innerText = jokeJS3.data.setup;
+        p6.innerText = jokeJS3.data.punchline;
+    } catch (err) {
+        console.log(err);
+    }
+}
+jokeFunc();
+
 // 4
 const fourth = document.querySelector('#fourth');
 const p7 = document.createElement('p');
 fourth.append(p7);
+
+const id = "38963";
+const season = "1";
+const number = "8";
+
+const fullURL = `http://api.tvmaze.com/shows/${id}/episodebynumber?season=${season}&number=${number}`;
+
+async function tvMazeFunc(){
+    try {
+        const mando = await axios.get(fullURL);
+        p7.innerText = mando.data.name;
+    } catch (err) {
+        console.log(err);
+    }
+}
+tvMazeFunc();
